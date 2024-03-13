c5dcd95cd20a22e594fe2f5c6a4f2af3534f37e1
George Taylor <3026030+georgepstaylor@users.noreply.github.com>
2024-03-13 12:53:22 +0000

    add dkim

--- terraform/dns_simplelogin.tf
+++ terraform/dns_simplelogin.tf
@@ -1,17 +1,17 @@
-resource "cloudflare_record" "simplelogin_verification" {
-  name    = var.SIMPLELOGIN_ANONYMOUS_DOMAIN
-  proxied = false
-  ttl     = 1
-  type    = "TXT"
-  value   = "sl-verification=qwmujktghmdbgjjlfrqmnabavbuvzu"
-  zone_id = data.cloudflare_zone.simplelogin.id
-}
+# resource "cloudflare_record" "simplelogin_verification" {
+#   name    = var.SIMPLELOGIN_ANONYMOUS_DOMAIN
+#   proxied = false
+#   ttl     = 60
+#   type    = "TXT"
+#   value   = "sl-verification=qwmujktghmdbgjjlfrqmnabavbuvzu"
+#   zone_id = data.cloudflare_zone.simplelogin.id
+# }
 
 resource "cloudflare_record" "simplelogin_mx" {
   name     = var.SIMPLELOGIN_ANONYMOUS_DOMAIN
   priority = 10
   proxied  = false
-  ttl      = 1
+  ttl      = 600
   type     = "MX"
   value    = "mx1.simplelogin.co."
   zone_id  = data.cloudflare_zone.simplelogin.id
@@ -21,7 +21,7 @@ resource "cloudflare_record" "simplelogin_mx2" {
   name     = var.SIMPLELOGIN_ANONYMOUS_DOMAIN
   priority = 20
   proxied  = false
-  ttl      = 1
+  ttl      = 600
   type     = "MX"
   value    = "mx2.simplelogin.co."
   zone_id  = data.cloudflare_zone.simplelogin.id
@@ -30,7 +30,7 @@ resource "cloudflare_record" "simplelogin_mx2" {
 resource "cloudflare_record" "simplelogin_spf" {
   name    = var.SIMPLELOGIN_ANONYMOUS_DOMAIN
   proxied = false
-  ttl     = 1
+  ttl     = 600
   type    = "TXT"
   value   = "v=spf1 include:_spf.simplelogin.co ~all"
   zone_id = data.cloudflare_zone.simplelogin.id
@@ -39,8 +39,17 @@ resource "cloudflare_record" "simplelogin_spf" {
 resource "cloudflare_record" "simplelogin_dmarc" {
   name    = "_dmarc"
   proxied = false
-  ttl     = 1
+  ttl     = 600
   type    = "TXT"
-  value   = "v=DMARC1; p=quarantine"
+  value   = "v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s"
   zone_id = data.cloudflare_zone.simplelogin.id
+}
+
+resource "cloudflare_record" "dkim" {
+  count   = 3
+  name    = count.index == 0 ? "dkim._domainkey" : "dkim0${count.index + 1}._domainkey"
+  proxied = false
+  ttl     = 600
+  type    = "CNAME"
+  value   = count.index == 0 ? "dkim._domainkey.simplelogin.co." : "dkim0${count.index + 1}._domainkey.simplelogin.co."
 }
