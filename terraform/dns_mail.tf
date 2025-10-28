f328e887c23b94ac1f31e73bc501f6bd743f3cc4
George Taylor <hey@george.dev>
2025-10-28 13:54:20 +0000

    feat: proton verify

--- terraform/dns_mail.tf
+++ terraform/dns_mail.tf
@@ -1,8 +1,8 @@
-resource "cloudflare_record" "george_dev_spf" {
+resource "cloudflare_record" "george_dev_proton_verification" {
   name    = "george.dev"
   proxied = false
   ttl     = 1
   type    = "TXT"
-  value   = "v=spf1 include:spf.messagingengine.com include:amazonses.com ~all"
+  value   = "protonmail-verification=d87dd8341c600dc15ceaffa9a7fa0a47ca719575"
   zone_id = data.cloudflare_zone.george_dev.id
 }
